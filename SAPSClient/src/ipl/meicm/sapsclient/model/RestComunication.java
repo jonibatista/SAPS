2d517fefbc50de463fd3878a203480e1cf269a9f
César Ferreira <cesar.manuel.ferreira@gmail.com>
2011-12-14 23:37:04 +0000

    webservice call now using TLS

--- SAPSClient/src/ipl/meicm/sapsclient/model/RestComunication.java
+++ SAPSClient/src/ipl/meicm/sapsclient/model/RestComunication.java
@@ -1,5 +1,7 @@
 package ipl.meicm.sapsclient.model;
 
+import ipl.meicm.sapsclient.controller.EasySSLSocketFactory;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
@@ -10,15 +12,28 @@ import java.net.URLEncoder;
 import java.util.Map;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.params.ClientPNames;
 import org.apache.http.client.params.CookiePolicy;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.params.ConnManagerPNames;
+import org.apache.http.conn.params.ConnPerRouteBean;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpProtocolParams;
 import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HttpContext;
 import org.apache.http.util.EntityUtils;
@@ -38,16 +53,49 @@ public class RestComunication {
 	HttpPost httpPost = null;
 	HttpGet httpGet = null;
 	String webServiceUrl;
+	HttpParams myParams;
+	private ClientConnectionManager clientConnectionManager;
+
 
 	// The serviceName should be the name of the Service you are going to be
 	// using.
 	public RestComunication(String serviceName) {
-		HttpParams myParams = new BasicHttpParams();
+
+		// prepare for the https connection call this in the constructor of the
+		// class that does the connection if it's used multiple times
+		SchemeRegistry schemeRegistry = new SchemeRegistry();
+
+		// http scheme
+		schemeRegistry.register(new Scheme("http", PlainSocketFactory
+				.getSocketFactory(), 80));
+		// https scheme
+		schemeRegistry.register(new Scheme("https", new EasySSLSocketFactory(),
+				443));
+
+		myParams = new BasicHttpParams();
+
+		myParams.setParameter(ConnManagerPNames.MAX_TOTAL_CONNECTIONS, 1);
+		myParams.setParameter(ConnManagerPNames.MAX_CONNECTIONS_PER_ROUTE,
+				new ConnPerRouteBean(1));
+		myParams.setParameter(HttpProtocolParams.USE_EXPECT_CONTINUE, false);
+		HttpProtocolParams.setVersion(myParams, HttpVersion.HTTP_1_1);
+		HttpProtocolParams.setContentCharset(myParams, "utf8");
+
+		CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+		credentialsProvider.setCredentials(new AuthScope("yourServerHere.com",
+				AuthScope.ANY_PORT), new UsernamePasswordCredentials(
+				"YourUserNameHere", "UserPasswordHere"));
+		clientConnectionManager = new ThreadSafeClientConnManager(myParams,
+				schemeRegistry);
 
 		HttpConnectionParams.setConnectionTimeout(myParams, 10000);
 		HttpConnectionParams.setSoTimeout(myParams, 10000);
-		httpClient = new DefaultHttpClient(myParams);
+		httpClient = new DefaultHttpClient(clientConnectionManager,myParams);
 		localContext = new BasicHttpContext();
+		
+		localContext.setAttribute("http.auth.credentials-provider",
+				credentialsProvider);
+		
 		webServiceUrl = serviceName;
 
 	}
